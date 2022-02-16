c1660cb043cc9438c4021e7e800c893e201667f8
Personal <personal@433215083NB001.local>
2022-02-16 15:51:16 +0000

    chore: fix formatting

--- public/app.js
+++ public/app.js
@@ -4,43 +4,43 @@ const keyboard = document.querySelector('.key-container')
 const wordle = 'SUPER'
 
 const keys = [
-    'Q',
-    'W',
-    'E',
-    'R',
-    'T',
-    'Y',
-    'U',
-    'I',
-    'O',
-    'P',
-    'A',
-    'S',
-    'D',
-    'F',
-    'G',
-    'H',
-    'J',
-    'K',
-    'L',
-    'ENTER',
-    'Z',
-    'X',
-    'C',
-    'V',
-    'B',
-    'N',
-    'M',
-    '«',
+	'Q',
+	'W',
+	'E',
+	'R',
+	'T',
+	'Y',
+	'U',
+	'I',
+	'O',
+	'P',
+	'A',
+	'S',
+	'D',
+	'F',
+	'G',
+	'H',
+	'J',
+	'K',
+	'L',
+	'ENTER',
+	'Z',
+	'X',
+	'C',
+	'V',
+	'B',
+	'N',
+	'M',
+	'«',
 ]
 
 const guessRows = [
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', ''],
-    ['', '', '', '', '']
+	['', '', '', '', ''],
+	['', '', '', '', ''],
+	['', '', '', '', ''],
+	['', '', '', '', ''],
+	['', '', '', '', ''],
+	['', '', '', '', '']
 ]
 
 let currentRow = 0
@@ -48,56 +48,56 @@ let currentTile = 0
 let isGameOver = false
 
 guessRows.forEach((guessRow,guessRowIndex) => {
-    const rowElement = document.createElement('div')
-    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
-    guessRow.forEach((guess, guessIndex) => {
-        const tileElement = document.createElement('div')
-        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
-        tileElement.classList.add('tile')
-        rowElement.append(tileElement)
-    })
-    tileDisplay.append(rowElement)
+	const rowElement = document.createElement('div')
+	rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+	guessRow.forEach((guess, guessIndex) => {
+		const tileElement = document.createElement('div')
+		tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+		tileElement.classList.add('tile')
+		rowElement.append(tileElement)
+	})
+	tileDisplay.append(rowElement)
 })
 
 keys.forEach(key => {
-    const buttonElement = document.createElement('button')
-    buttonElement.textContent = key
-    buttonElement.setAttribute('id', key)
-    buttonElement.addEventListener('click', () => handleClick(key))
-    keyboard.append(buttonElement)
+	const buttonElement = document.createElement('button')
+	buttonElement.textContent = key
+	buttonElement.setAttribute('id', key)
+	buttonElement.addEventListener('click', () => handleClick(key))
+	keyboard.append(buttonElement)
 })
 
 const handleClick = (letter) =>{
-    console.log('clicked', letter)
-    if(letter === '«') {
-        deleteLetter()
-        return
-    }
+	console.log('clicked', letter)
+	if(letter === '«') {
+		deleteLetter()
+		return
+	}
 
-    if(letter === 'ENTER') {
-        console.log('check row')
-        return
-    }
-    addLetter(letter)
+	if(letter === 'ENTER') {
+		console.log('check row')
+		return
+	}
+	addLetter(letter)
 }
 
 const addLetter = (letter) =>{
-    if(currentTile < 5 && currentRow < 6){
-        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
-        tile.textContent = letter
-        guessRows[currentRow][currentTile] = letter
-        tile.setAttribute('data', letter)
-        currentTile++
-        console.log('guessRows', guessRows)
-    }
+	if(currentTile < 5 && currentRow < 6){
+		const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+		tile.textContent = letter
+		guessRows[currentRow][currentTile] = letter
+		tile.setAttribute('data', letter)
+		currentTile++
+		console.log('guessRows', guessRows)
+	}
 }
 
 const deleteLetter = () => {
-    if(currentTile>0){
-        currentTile--
-        const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
-        tile.textContent = ''
-        guessRows[currentRow][currentTile] = ''
-        tile.setAttribute('data', '')
-    }
-}
+	if(currentTile>0){
+		currentTile--
+		const tile = document.getElementById('guessRow-' + currentRow + '-tile-' + currentTile)
+		tile.textContent = ''
+		guessRows[currentRow][currentTile] = ''
+		tile.setAttribute('data', '')
+	}
+}   
